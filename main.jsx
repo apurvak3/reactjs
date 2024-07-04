d1da717c654a1ab49fa0689a21b1be35286c624a
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-05 02:01:38 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -10,7 +10,7 @@ return(
   </div>
 )
 }
-const reactElement = {
+/*const reactElement = {
   type: 'a',
   props : { 
     href : 'https://google.com',
@@ -22,7 +22,15 @@ children : 'click me to visit google',
 }
 const anotherElement=(
   <a href ="https://google.com" target='_blank'>Visit google</a>   //object is predefined i.e <any tag, attribute>text
-)
+)*/
+const anotherElement = 'Welcome to the page'
+const reactElement = React.createElement(
+  'a',
+  {
+    href :"https://google.com",target :'_blank'
+  },
+  'click me to visit google',anotherUser
+  )
 
 
 ReactDOM.createRoot(document.getElementById('root')).

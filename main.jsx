7b79ebfbbf2f6b8a59fb2aed2efb891e863d8c68
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-05 08:58:39 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-
-function MyApp(){
-return(
-  <div>
-    <h1>Custom APP</h1>
-  </div>
-)
+function MyApp() {   //This is a functional component that returns a simple JSX element containing a div with a heading saying "Custom APP".
+  return (
+    <div>
+      <h1>Custom APP</h1>
+    </div>
+  );
 }
-/*const reactElement = {
-  type: 'a',
-  props : { 
-    href : 'https://google.com',
-    target:'_blank',
-    
-},
-children : 'click me to visit google',
 
-}
-const anotherElement=(
-  <a href ="https://google.com" target='_blank'>Visit google</a>   //object is predefined i.e <any tag, attribute>text
-)*/
-const anotherElement = 'Welcome to the page'
-const reactElement = React.createElement(
+
+const anotherElement = (    //This is a JSX element. It creates an anchor (<a>) tag that links to Google and opens in a new tab. The text displayed for the link is "Visit Google".
+  <a href="https://google.com" target="_blank">Visit Google</a>
+);
+
+
+const reactElement = React.createElement(  //This creates the same anchor tag as above, but using React.createElement instead of JSX.
   'a',
   {
-    href :"https://google.com",target :'_blank'
+    href: "https://google.com",
+    target: '_blank'
   },
-  'click me to visit google',anotherUser
-  )
+  '  (hey!click me to visit google)'
+);
 
-
-ReactDOM.createRoot(document.getElementById('root')).
-render(
-  anotherElement
-)
+ReactDOM.createRoot(document.getElementById('root')).render(// This initializes a React root container at the DOM element with the id root.
+  <React.StrictMode>
+    {anotherElement}
+    {reactElement}
+    <MyApp />
+  </React.StrictMode>
+);
 //wwe are not able to render react element but we are able to render another element.conversion takes place
+//summary
+Import necessary React libraries and custom components.
+Define a simple functional component (MyApp) that returns some JSX.
+Create a JSX element (anotherElement) and a React element (reactElement) that both represent anchor tags linking to Google.
+Render the anotherElement, reactElement, and MyApp component to the root DOM element using ReactDOM.createRoot().render().
+
